d54161391592962379c2798199116ee909a34987
Neo <her_liu@qq.com>
2021-09-05 07:35:02 +0800

    finished

---
+++ config.js
@@ -0,0 +1,8 @@
+
+// export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+// export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+
+
+export const nftmarketaddress = "0x41a6c42E4f6D77e461195BD448116fdEA8B8989e"
+export const nftaddress = "0xA82E7060a8bD3CEe464437BFdb0Ccf7Cd9c04387"
+
